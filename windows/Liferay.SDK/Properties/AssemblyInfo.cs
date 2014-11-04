1b333dc522abf0e96412d982c60f49ac548ea361
Andrea Di Giorgi <andrea.digiorgi@smc.it>
2014-11-03 17:03:06 -0800

    Added comments in AssemblyInfo

--- windows/Liferay.SDK/Properties/AssemblyInfo.cs
+++ windows/Liferay.SDK/Properties/AssemblyInfo.cs
@@ -1,11 +1,25 @@
 using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyTitleAttribute("Liferay SDK for Windows")]
-[assembly: AssemblyDescriptionAttribute("")]
-[assembly: AssemblyProductAttribute("Liferay SDK for Windows")]
-[assembly: AssemblyCopyrightAttribute("Copyright (c) 2014 Andrea Di Giorgi. All rights reserved.")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Liferay SDK for Windows")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Liferay SDK for Windows")]
+[assembly: AssemblyCopyright("Copyright (c) 2014 Andrea Di Giorgi. All rights reserved.")]
+[assembly: AssemblyTrademarkAttribute("Liferay is a registered trademark of Liferay, Inc.")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersionAttribute("1.0.0.0")]
 [assembly: AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en")]
-[assembly: AssemblyTrademarkAttribute("Liferay is a registered trademark of Liferay, Inc.")]
