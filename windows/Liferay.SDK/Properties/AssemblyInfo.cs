5c7ba7ef8b015f117d97ff77833cc47563a8cd5c
Andrea Di Giorgi <andrea.digiorgi@smc.it>
2014-11-03 16:27:58 -0800

    Commit C# solution files

---
+++ windows/Liferay.SDK/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitleAttribute("Liferay SDK for Windows")]
+[assembly: AssemblyDescriptionAttribute("")]
+[assembly: AssemblyProductAttribute("Liferay SDK for Windows")]
+[assembly: AssemblyCopyrightAttribute("Copyright (c) 2014 Andrea Di Giorgi. All rights reserved.")]
+[assembly: AssemblyVersionAttribute("1.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
+[assembly: AssemblyTrademarkAttribute("Liferay is a registered trademark of Liferay, Inc.")]
