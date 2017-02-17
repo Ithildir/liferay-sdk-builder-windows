7ef4081ad3f06fabccb058c8fc63c5665d91b8f3
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-02-17 08:51:58 -0800

    Move to MIT license, fixes #5

--- windows/Liferay.SDK/Properties/AssemblyInfo.cs
+++ windows/Liferay.SDK/Properties/AssemblyInfo.cs
@@ -1,7 +1,28 @@
-using System.Reflection;
-using System.Resources;
-
-// General Information about an assembly is controlled through the following
+//------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs">
+//    Copyright (c) 2014-present Andrea Di Giorgi
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a
+//    copy of this software and associated documentation files (the "Software"),
+//    to deal in the Software without restriction, including without limitation
+//    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//    and/or sell copies of the Software, and to permit persons to whom the
+//    Software is furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in
+//    all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//    DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <author>Andrea Di Giorgi</author>
+// <website>https://github.com/Ithildir/liferay-sdk-builder-windows</website>
+//------------------------------------------------------------------------------ut an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Liferay SDK for Windows")]
@@ -9,7 +30,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Andrea Di Giorgi")]
 [assembly: AssemblyProduct("Liferay SDK for Windows")]
-[assembly: AssemblyCopyright("Copyright (c) 2014 Andrea Di Giorgi. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2014-present Andrea Di Giorgi. All rights reserved.")]
 [assembly: AssemblyTrademark("Liferay is a registered trademark of Liferay, Inc.")]
 [assembly: NeutralResourcesLanguage("en")]
 
